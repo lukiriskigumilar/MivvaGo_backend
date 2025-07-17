a4187f6919bd48555db2fd1fa80c49ace5774de1
Luki riski <contactlukiriski@gmail.com>
2025-07-17 17:15:27 +0700

    Initial proejct

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import cors from 'cors';
+
+dotenv.config();
+
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
 
