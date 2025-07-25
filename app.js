8a5526191f6a0f235899b45a823053c6d3f367d2
Luki Riski <contactlukiriski@gmail.com>
2025-07-25 15:06:45 +0700

    add library express user agent and add some pull db

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
+import useragent from 'express-useragent';
 import cors from 'cors';
 dotenv.config();
 
@@ -17,6 +18,7 @@ app.use(validateContentTypeOnlyJson);
 
 app.use(express.json());
 app.use(helmet());
+app.use(useragent.express())
 // app.use(cors());
 
 app.use('/api/v1/auth/user', userAuthRoutes);
