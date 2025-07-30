2b476d80f67147a85e2c2c2f41f84fe0fd0efc05
Luki Riski <contactlukiriski@gmail.com>
2025-07-31 02:40:41 +0700

    fix: change parameter to access token and add delete cookie refresh and access

--- src/controllers/auth/user/logout_user_controller.js
+++ src/controllers/auth/user/logout_user_controller.js
@@ -2,10 +2,15 @@ import logoutSessionUserService from "../../../services/auth/user/logout_service
 import { errorResponse, successResponse } from "../../../utils/custom_response.js";
 
 const logoutUserController = async (req, res) => {
-  const id_user = req.user.id;
+  const accessToken = req.accessToken;
   try {
-    const result = await logoutSessionUserService(id_user);
-    successResponse(res, result.message, {}, result.statusCode);
+    const result = await logoutSessionUserService(accessToken);
+        res.setHeader("Set-Cookie", [
+            result.deletedAccessToken,
+            result.deletedRefreshToken,
+        ]);
+       
+        successResponse(res, result.message, {}, result.statusCode);
   } catch (error) {
     errorResponse(
       res,
--- src/services/auth/user/logout_service.js
+++ src/services/auth/user/logout_service.js
@@ -2,16 +2,16 @@ import redisClient from "../../../config/redis/redis.js";
 import findSessionUserRepository from "../../../repository/auth/user/find_session_user_repository.js";
 import updateSessionUserRepository from "../../../repository/auth/user/update_session_user_repository.js";
 import AppError from "../../../utils/appError.js";
+import { createCookieString } from "../../../utils/cookie_helper.js";
 
-const logoutSessionUserService = async (id_user) => {
+const logoutSessionUserService = async (accessToken) => {
   const findUser = {
-    user_id:id_user
+   access_token:accessToken
   }
   const sessionUser = await findSessionUserRepository(findUser);
   if (!sessionUser) {
     throw new AppError("You're not logged in.", 401);
   }
-  const accessToken = sessionUser.access_token;
   const dataToUpdate = {
     is_revoked: true,
     refresh_token_expiry: new Date(),
@@ -28,10 +28,27 @@ const logoutSessionUserService = async (id_user) => {
     ttlInSecond
   );
 
+  const deletedAccessToken = createCookieString("accessToken","",{
+    maxAge:0,
+    httpOnly: true,
+    secure: true,
+    path: "/",
+    sameSite: "strict",
+  })
+  const deletedRefreshToken = createCookieString("refreshToken","",{
+    maxAge:0,
+    httpOnly: true,
+    secure: true,
+    path: "/",
+    sameSite: "strict",
+  })
+
   if (updateSession && insertBlacklistAccessToken === "OK") {
     return {
-      statusCode: 200,
-      message: "Logout successful. Access token has been blacklisted.",
+        deletedAccessToken,
+        deletedRefreshToken,
+        statusCode: 200,
+        message: "Logout successful. Access token has been blacklisted.",
     };
   } else {
     throw new AppError("Logout failed due to a server error.", 500);
