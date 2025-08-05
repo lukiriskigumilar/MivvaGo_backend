3dd1dc3810fd8179814d0382541d0c386463675c
Luki Riski <contactlukiriski@gmail.com>
2025-08-05 14:24:15 +0700

    Fix: logic apps middleware

--- src/middlewares/validateContentType.js
+++ src/middlewares/validateContentType.js
@@ -1,27 +1,29 @@
 /**
  * Middleware to enforce 'Content-Type: application/json' for specific HTTP methods
  * Applies to POST, PUT, PATCH, DELETE
- * 
+ *
  * If the content type is not JSON, returns a 415 Unsupported Media Type error.
  */
 
 const validateContentType = (req, res, next) => {
-    const method = req.method; 
-    const contentType = req.headers['content-type'];
-     const hasBodyContent = req.body && Object.keys(req.body).length > 0;
+  const method = req.method;
+  const contentType = req.headers["content-type"];
+  const methodsToCheck = ["POST", "PUT", "PATCH"];
+  const contentLength = parseInt(req.headers["content-length"], 10) || 0;
 
-    const methodsToCheck = ['POST', 'PUT', 'PATCH'];
-
-    if(methodsToCheck.includes(method) && hasBodyContent){
-        if(!contentType || !contentType.includes('application/json')) {
-            return res.status(415).json({
-                statusCode: 415,
-                status: 'error',
-                message: 'Unsupported Media Type. Content-Type must be application/json',
-            });
-        }
+  if (methodsToCheck.includes(method) && contentLength > 0) {
+    if (methodsToCheck.includes(method)) {
+      if (!contentType || !contentType.includes("application/json")) {
+        return res.status(415).json({
+          statusCode: 415,
+          status: "error",
+          message:
+            "Unsupported Media Type. Content-Type must be application/json",
+        });
+      }
     }
-    next();
-}
+  }
+  next();
+};
 
 export default validateContentType;
