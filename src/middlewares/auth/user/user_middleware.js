dcbed7de1d41298690c7dfa9b115e9c9f6aa1aa0
Luki Riski <contactlukiriski@gmail.com>
2025-08-05 15:25:46 +0700

    add middleware : validate logout and validate get access Token

--- src/middlewares/auth/user/user_middleware.js
+++ src/middlewares/auth/user/user_middleware.js
@@ -6,7 +6,8 @@ import authValidator from "../user/user_validator.js";
 import { errorResponse } from "../../../utils/custom_response.js";
 import redisClient from "../../../config/redis/redis.js";
 import AppError from "../../../utils/appError.js";
-
+import findSessionUserRepository from "../../../repository/auth/user/find_session_user_repository.js";
+import cookieParser from "cookie-parser";
 
 const validateUserRegistration = (req, res, next) => {
   const { error } = authValidator.registerSchema.validate(req.body, {
@@ -29,7 +30,6 @@ const validateResendVerifyEmail = (req, res, next) => {
   const { error } = authValidator.resendVerifyEmailSchema.validate(req.body, {
     abortEarly: false,
   });
-
   if (error) {
     const errorMessages = error.details.map((detail) => detail.message);
     return errorResponse(
@@ -59,7 +59,12 @@ const validateLoginEmail = (req, res, next) => {
   if (accessToken) {
     jwt.verify(accessToken, process.env.JWT_SECRET, (err, user) => {
       if (!err) {
-        return errorResponse(res, "You're already logged in", {error:"You're already logged in"}, 409);
+        return errorResponse(
+          res,
+          "You're already logged in",
+          { error: "You're already logged in" },
+          409
+        );
       }
       next();
     });
@@ -68,35 +73,84 @@ const validateLoginEmail = (req, res, next) => {
   }
 };
 
-const validateCredentialUser = async (req, res , next) =>{
-  const accessToken = req.cookies.accessToken; 
-  if(!accessToken){
-    return errorResponse(res,"AccessToken is Required", {error:"Missing accessToken"}, 401)
+const validateGetAccessToken = async (req, res, next) => {
+  const refreshToken = req.cookies.refreshToken;
+  if (!refreshToken) {
+    return errorResponse(
+      res,
+      "refresh token invalid",
+      { refresh_token: "Refresh token is required" },
+      401
+    );
+  }
+  req.refreshToken = refreshToken;
+  next();
+};
+
+const validateCredentialUser = async (req, res, next) => {
+  const accessToken = req.cookies.accessToken;
+  if (!accessToken) {
+    return errorResponse(
+      res,
+      "AccessToken is Required",
+      { error: "Missing accessToken" },
+      401
+    );
   }
   try {
-    const decode = await jwt.verify(accessToken, process.env.JWT_SECRET); 
-    const isBlackListed = await redisClient.get(`blacklist_access_token:${accessToken}`)
-    if(isBlackListed){
-      return errorResponse(res, "Token has been blacklisted", { error: "blacklisted_token" }, 401);
+    const decode = await jwt.verify(accessToken, process.env.JWT_SECRET);
+    const isBlackListed = await redisClient.get(
+      `blacklist_access_token:${accessToken}`
+    );
+    if (isBlackListed) {
+      return errorResponse(
+        res,
+        "Token has been blacklisted",
+        { error: "blacklisted_token" },
+        401
+      );
     }
-    req.user = decode
-    req.accessToken = accessToken
-    next()
-
+    req.user = decode;
+    req.accessToken = accessToken;
+    next();
   } catch (error) {
-    if (error.name === 'TokenExpiredError'){
-      errorResponse(res, "Token expired", {error:"Token expired"}, 401)
-    }else{
-      errorResponse(res, "Invalid Token", {error:"Invalid_token"}, 401)
+    if (error.name === "TokenExpiredError") {
+      errorResponse(res, "Token expired", { error: "Token expired" }, 401);
+    } else {
+      errorResponse(res, "Invalid Token", { error: "Invalid_token" }, 401);
     }
-    
   }
+};
 
-}
+const validateLogoutUser = async (req, res, next) => {
+  const accessToken = req.cookies.accessToken;
+  if (!accessToken) {
+    return errorResponse(
+      res,
+      "missing access Token",
+      { error: "Access token cookies can not found" },
+      401
+    );
+  }
+  await jwt.verify(accessToken, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return errorResponse(
+        res,
+        "not Valid Credential",
+        { error: "You're access Token not valid" },
+        401
+      );
+    }
+    req.accessToken = accessToken
+    next();
+  });
+};
 
 export default {
   validateUserRegistration,
   validateResendVerifyEmail,
   validateLoginEmail,
-  validateCredentialUser
+  validateCredentialUser,
+  validateGetAccessToken,
+  validateLogoutUser,
 };
